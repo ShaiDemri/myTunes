16b1155986c4b71702cffc29907f62ae26da6923
Shai Demri <shai@vivus.ai>
2020-01-19 00:42:51 +0200

    Updates

---
+++ precache-manifest.11ff2cc29d20ad0e1beccc054a7152fb.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32a4256f9ae0f6d8ac6e816e14507962",
+    "url": "/myTunes/index.html"
+  },
+  {
+    "revision": "0707d8ac1456460de87c",
+    "url": "/myTunes/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "763c42ade785408a8c80",
+    "url": "/myTunes/static/js/0.c683c80e.chunk.js"
+  },
+  {
+    "revision": "fcd03f45be1a8118b073",
+    "url": "/myTunes/static/js/1.985c9660.chunk.js"
+  },
+  {
+    "revision": "9721b6269c27c1b673ac",
+    "url": "/myTunes/static/js/10.9199fe3c.chunk.js"
+  },
+  {
+    "revision": "a1a17932848370199ee4",
+    "url": "/myTunes/static/js/11.063f5945.chunk.js"
+  },
+  {
+    "revision": "08db6bb7048c4cd3fb29",
+    "url": "/myTunes/static/js/12.bd14e269.chunk.js"
+  },
+  {
+    "revision": "29fedb2bf32e71dd0f88",
+    "url": "/myTunes/static/js/13.bcb4e867.chunk.js"
+  },
+  {
+    "revision": "5bf93988ce3dcb144ba3",
+    "url": "/myTunes/static/js/2.a430e151.chunk.js"
+  },
+  {
+    "revision": "2ed3fa41a53d03dd63df",
+    "url": "/myTunes/static/js/3.80591797.chunk.js"
+  },
+  {
+    "revision": "d99c7416e3c5fa43042a",
+    "url": "/myTunes/static/js/6.a75a89ff.chunk.js"
+  },
+  {
+    "revision": "53b9227046030e7092d1",
+    "url": "/myTunes/static/js/7.b8e0020f.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/myTunes/static/js/7.b8e0020f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "601de6b8643aeb53a5c8",
+    "url": "/myTunes/static/js/8.6e883ea8.chunk.js"
+  },
+  {
+    "revision": "42d6e34b433baeac35fc",
+    "url": "/myTunes/static/js/9.5092e1a1.chunk.js"
+  },
+  {
+    "revision": "0707d8ac1456460de87c",
+    "url": "/myTunes/static/js/main.991c3f56.chunk.js"
+  },
+  {
+    "revision": "8d22933acd7f77d28d4a",
+    "url": "/myTunes/static/js/runtime-main.b11f2f5e.js"
+  }
+]);
